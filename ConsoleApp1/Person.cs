1ef2e72e8d3ea9bfe486b2b4f90e86cb235b5346
Joshua Thomas <guitarrocks10@yahoo.com>
2019-12-03 16:31:32 -0500

    Add project files.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+        public static double SumOfAllAges;
+
+        
+        public void PersonInfo()
+        {
+            System.Console.WriteLine("First Name?");
+            FirstName = System.Console.ReadLine();
+            System.Console.WriteLine("Last Name?");
+            LastName = System.Console.ReadLine();
+            System.Console.WriteLine("Age?");
+            Age = int.Parse(System.Console.ReadLine());
+            SumOfAllAges += this.Age;
+
+            
+        }
+
+        public void SpouseInfo()
+        {
+            Spouse = new Person();
+            System.Console.WriteLine("Spouse Name?");
+            Spouse.FirstName = System.Console.ReadLine();
+            Spouse.LastName = LastName;
+            System.Console.WriteLine("Spouse Age?");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+            SumOfAllAges += Spouse.Age;
+            
+        }
+        private string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine("Name: " + this.GetFullName());
+            System.Console.WriteLine("Age: " + this.Age);
+            System.Console.WriteLine("Spouse Name: " + Spouse.GetFullName());
+            System.Console.WriteLine("Spouse Age: " + Spouse.Age);
+            
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person P1 = new Person();
+            P1.PersonInfo();
+            P1.SpouseInfo();
+            Person P2 = new Person();
+            P2.PersonInfo();
+            P2.SpouseInfo();
+
+            P1.PrintNameAndAge();
+            P2.PrintNameAndAge();
+            System.Console.WriteLine("Adverage Age:" + Person.SumOfAllAges / 4);
+
+            System.Console.ReadKey();
+        }
+        
+    }
+}
